bbe248ded6436468b48f20b8518553d46bd46bfc
Johanna Flato <jcflato@gmail.com>
2018-05-20 13:45:43 +0100

    add latin translation; tidied print functions

--- lipsum_multisource.py
+++ lipsum_multisource.py
@@ -1,7 +1,10 @@
+# coding: utf-8
 import markovify
 import glob
 from numpy import random
+from googletrans import Translator
 
+# randomly sample from sources.
 sourcelist = []
 sourcelist_sample = []
 
@@ -18,9 +21,7 @@ def print_sources():
     print ' + '.join(sourcelist_sample_toprint) + ':' + '\n'
 
 
-print_sources()
-
-# Get raw text as string; build sampled models.
+# get raw text as string; build sampled models.
 markoved_texts = []
 
 for s in sourcelist_sample:
@@ -28,7 +29,7 @@ for s in sourcelist_sample:
         text_model = markovify.Text(f)
         markoved_texts.append(text_model)
 
-# Build the composite model.
+# build the composite model.
 lipsum_model = markovify.combine(markoved_texts)
 
 n_sent = random.randint(8, 12)
@@ -37,6 +38,22 @@ sent_chain = []
 for i in range(n_sent):
     sent_chain.append(lipsum_model.make_sentence())
 
-new_text = ' '.join(sent_chain)
+en_text = ' '.join(sent_chain)
 
-print new_text
+# translate to latin.
+translator = Translator()
+translation = translator.translate(en_text, dest='la')
+la_text = translation.text
+
+
+# print english text and latin text.
+def print_texts():
+    print en_text
+    print '<-->'
+    print la_text
+
+
+# to do: replace untranslatable words with 'lorem'. to do so: convert to array...run all through '.detect' command...if not 'latin', replace with 'lorem'   # noqa: E501
+
+print_sources()
+print_texts()
